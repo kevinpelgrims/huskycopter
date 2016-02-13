c643217dd0bea44bcf0890e4ad6f136ba6f4ec67
Kevin Pelgrims <kevin.pelgrims@gmail.com>
2016-02-13 22:49:57 +0100

    Play sound from MainActivity
    Stop the sound when pausing the activity.

--- app/src/main/java/com/kevinpelgrims/smile/MainActivity.java
+++ app/src/main/java/com/kevinpelgrims/smile/MainActivity.java
@@ -4,9 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private SoundManager soundManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        soundManager = new SoundManager(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        soundManager.playHusky();
+    }
+
+    @Override
+    protected void onPause() {
+        soundManager.stopHusky();
+        super.onPause();
     }
 }
